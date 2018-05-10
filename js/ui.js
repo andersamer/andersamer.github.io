b979228d020c17f70fb2db2199c3b72a92de857f
Sam Andersen <sam.jon.andersen@gmail.com>
2018-05-10 10:15:02 -0700

    Type v2

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@ function print(str, md=true) {
     var txt;
     if(md) { txt = marked(str) }
     else { txt = str }
-    $txt.html($txt.html() + str);
+    $txt.html($txt.html() + txt);
 }
 
 function println(str, md) {
